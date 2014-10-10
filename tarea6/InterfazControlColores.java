d0395be0ae88d50c618b3e877dd576649215b8a0
EmanuelGP <gpe_fc@uaem.mx>
2014-10-09 23:13:09 -0500

    Interfaz completa.

--- tarea6/InterfazControlColores.java
+++ tarea6/InterfazControlColores.java
@@ -4,6 +4,21 @@ import javax.swing.*;
 
 public class InterfazControlColores extends JFrame{
 
+	private JLabel etiquetaRojoJL;
+	private JLabel etiquetaVerdeJL;
+	private JLabel etiquetaAzulJL;
+
+	private JButton botonColorJB;
+
+	private JTextField valorRojoJTF;
+	private JTextField valorVerdeJTF;
+	private JTextField valorAzulJTF;
+	private JTextField textoColorJTF;
+
+	private Color colorRojoC = Color.RED;
+	private Color colorVerdeC = Color.GREEN;
+	private Color colorAzul = Color.BLUE;
+
 	private JSlider rangoRojoJS;
 	private JSlider rangoVerdeJS;
 	private JSlider rangoAzulJS;
@@ -15,6 +30,19 @@ public class InterfazControlColores extends JFrame{
 	public InterfazControlColores(){
 
 		super("Selector de Color");
+
+		etiquetaRojoJL = new JLabel("Rojo:");
+		etiquetaVerdeJL = new JLabel("Verde:");
+		etiquetaAzulJL = new  JLabel("Azul:");
+
+		botonColorJB = new JButton("Aplicar color");
+
+		valorRojoJTF = new JTextField(5);
+		valorVerdeJTF = new JTextField(5);
+		valorAzulJTF = new JTextField(5);
+		textoColorJTF = new JTextField("Selector de color con slider's");
+		//textoColorJTF.setFont(Font.BOLD);
+		textoColorJTF.setEditable(false);
 
 		rangoRojoJS = new JSlider(SwingConstants.HORIZONTAL, 0, 255, 0);
 		rangoRojoJS.setMajorTickSpacing(25);
@@ -32,8 +60,16 @@ public class InterfazControlColores extends JFrame{
 		colorSlider.add(rangoAzulJS);
 
 		panelDatos = new JPanel();
-		panelDatos.setBackground(Color.BLACK);
-		panelDatos.setPreferredSize(new Dimension(350,50));
+		panelDatos.add(etiquetaRojoJL);
+		panelDatos.add(valorRojoJTF);
+		panelDatos.add(etiquetaVerdeJL);
+		panelDatos.add(valorVerdeJTF);
+		panelDatos.add(etiquetaAzulJL);
+		panelDatos.add(valorAzulJTF);
+		panelDatos.add(textoColorJTF);
+		panelDatos.add(botonColorJB);
+		//panelDatos.setBackground(Color.CYAN);
+		panelDatos.setPreferredSize(new Dimension(350,60));
 
 		add(colorSlider);
 		add(panelDatos, BorderLayout.SOUTH);
@@ -44,19 +80,19 @@ public class InterfazControlColores extends JFrame{
 			super.paintComponent(colorSlider);
 			
 			// establece nuevo color de dibujo, usando valores enteros
-			colorSlider.setColor( new Color( 255, 0, 0 ) );
-			colorSlider.fillRect( 25, 5, 40, 20 );
+			colorSlider.setColor(colorRojoC);
+			colorSlider.fillRect(25, 5, 40, 20);
 			
 			// establece nuevo color de dibujo, usando valores de punto flotante
-			colorSlider.setColor( new Color( 0.50f, 0.75f, 0.0f ) );
-			colorSlider.fillRect( 25, 36, 40, 20 );
+			colorSlider.setColor(colorVerdeC);
+			colorSlider.fillRect(25, 36, 40, 20);
 			
 			// establece nuevo color de dibujo, usando objetos Color static
-			colorSlider.setColor( Color.BLUE );
-			colorSlider.fillRect( 25, 68, 40, 20 );
+			colorSlider.setColor(colorAzul);
+			colorSlider.fillRect(25, 68, 40, 20);
 
-			colorSlider.setColor( new Color( 25, 50, 0 ) );
-			colorSlider.fillRect( 150, 110, 120, 50 );
+			colorSlider.setColor(new Color( 25, 50, 0 ));
+			colorSlider.fillRect(150, 110, 120, 50);
 			colorSlider.drawString("Generado", 75, 138);
 		}
 	}
--- tarea6/SelectorColores.java
+++ tarea6/SelectorColores.java
@@ -4,7 +4,7 @@ public class SelectorColores{
 	public static void main(String args[]){
 		InterfazControlColores selectorColores = new InterfazControlColores();
 		selectorColores.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		selectorColores.setSize(350, 250);
+		selectorColores.setSize(350, 260);
 		selectorColores.setVisible(true);
 	}
 }
